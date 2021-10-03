849dc9b41144f5e6a34a4d387bd303096190ca42
Vitor Dubiela <vitorhugo.dubiela@gmail.com>
2021-10-03 19:33:19 -0300

    Test update function

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/", async (req, res) => {
         }
     );
 
-    const totalClassesString = await googleSheets.spreadsheets.values.get({
+    const totalClassesCell = await googleSheets.spreadsheets.values.get({
         spreadsheetId: sheetId,
         range: "engenharia_de_software!A2:A2"
     }).then((value) => {
@@ -36,7 +36,10 @@ app.get("/", async (req, res) => {
         console.log(reason);
     });
 
-    const studentsInfo = await googleSheets.spreadsheets.values.get({
+    const classesNmb = getTotalClasses(totalClassesCell);
+    const allowedAbs = classesNmb * 0.25;
+
+    const studentsData = await googleSheets.spreadsheets.values.get({
         spreadsheetId: sheetId,
         range: "engenharia_de_software!A4:F"
     }).then((value) => {
@@ -45,26 +48,56 @@ app.get("/", async (req, res) => {
         console.log(reason);
     });
 
-    for (const row of studentsInfo) {
-        let id = row[0];
-        let ab = row[2];
-        let p1 = row[3];
-        let p2 = row[4];
-        let p3 = row[5];
-        console.log(`ID: ${id} / Absences: ${ab} / P1: ${p1} / P2: ${p2} / P3: ${p3}\n`);
+    const totalStudentsNmb = studentsData.length;
+
+    function printRows(sheetRows, classesNmb) {
+        for (const row of sheetRows) {
+            let id = row[0];
+            let abs = row[2];
+            let p1 = row[3];
+            let p2 = row[4];
+            let p3 = row[5];
+            console.log(`ID: ${id} / Absences: ${abs} / P1: ${p1} / P2: ${p2} / P3: ${p3}\n`);
+            if (abs > allowedAbs) {
+
+            }
+        }
+        console.log(classesNmb);
+        console.log(totalStudentsNmb);
+    }
+    printRows(studentsData, classesNmb);
+
+    async function updateAbsences() {
+        await googleSheets.spreadsheets.values.update({
+            auth: client,
+            spreadsheetId: sheetId,
+            range: "engenharia_de_software!G4",
+            valueInputOption: 'USER_ENTERED',
+            requestBody: {
+                range: "engenharia_de_software!G4",
+                values: [
+                    ["aprovado"]
+                ]
+            }
+        }).then((value) => {
+            console.log(value);
+        }, (err) => {
+            console.log(err);
+        });
     }
 
-    console.log(getTotalClasses(totalClassesString));
-    res.send(JSON.stringify(studentsInfo));
+    updateAbsences();
+    res.send(JSON.stringify(studentsData));
 });
+
 
 function getTotalClasses(classesStr) {
     let str = classesStr[0][0];
     let totalClasses = str.substring(str.indexOf(':') + 1);
 
-    
     return parseInt(totalClasses);
 }
+
 
 const port = 8080;
 const hostname = "localhost";
